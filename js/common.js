36e11b242a0b6b22db43b6eeb6a8aab5acc3c06e
onbgh823 <68522421+onbgh823@users.noreply.github.com>
2020-09-10 17:24:22 +0900

    common logo

--- js/common.js
+++ js/common.js
@@ -8,7 +8,13 @@
         $('#leeContainer').remove()
         $('#leeBox').laod(url)
     })
+    // 로고부분
+    $('.logoNav > h1 > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#leeContainer').remove()
+        $('#leeBox').load(url)
+    })
 
-    
 
 })(jQuery)
