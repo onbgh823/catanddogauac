3e60c46512c65c1daa082014a7ea6fdec7afd3b1
onbgh823 <68522421+onbgh823@users.noreply.github.com>
2020-09-10 17:39:26 +0900

    로고부분
    mian.html -> index.html(원래대로

--- js/common.js
+++ js/common.js
@@ -9,12 +9,12 @@
         $('#leeBox').laod(url)
     })
     // 로고부분
-    $('.logoNav > h1 > a').on('click', function(e){
-        e.preventDefault()
-        var url = $(this).attr('href')
-        $('#leeContainer').remove()
-        $('#leeBox').load(url)
-    })
+    // $('.logoNav > h1 > a').on('click', function(e){
+    //     e.preventDefault()
+    //     var url = $(this).attr('href')
+    //     $('#leeContainer').remove()
+    //     $('#leeBox').load(url)
+    // })
 
 
 })(jQuery)
