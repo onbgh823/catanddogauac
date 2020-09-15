2c831f790f6107a88bd9d14cff83001a76992645
onbgh823 <68522421+onbgh823@users.noreply.github.com>
2020-09-15 10:07:46 +0900

    slick

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@
     // $(window).load(function(){
     //     $('.introAni').delay(500).fadeOut(500)
     // })
+    // 인덱스 introAni
     // setInterval 1초에 한번씩 가동 숫자를 -1씩 감소시키는 text method, clearInterval
     //setInterval을 1초에 한번씩 숫자를 -1씩 감소시키는 함수를 불러서 span에 감소된 숫자를 text()로 써줌. 숫자값이 0가 되면 clearInterval시키고, .introAni를 fadeOut(500)
 
@@ -20,7 +21,7 @@
     // }
 
     //글자색 랜덤, 활용가능
-    var colorCode =['red','blue','green','yellow','lime']
+    var colorCode =['red','blue','green','yellow','lime','default']
     var minCnt = setInterval(minusCount, 1000)
     var k = 5;
     function minusCount() {
